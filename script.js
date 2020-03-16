aa9b1d15c04091170a3d841436c6f6eb85174316
EvgeniyGuzenkov <jack199479@gmail.com>
2020-03-16 22:55:55 +0300

    feat: add a message after submiting the form

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ NAVIGATION.addEventListener("click", event => {
 
 //--------------------------------------------------PORTFOLIO BUTTONS----------------------------
 
-const IMAGES_SWITCHING = document.querySelector("div.portfolio__images-switching");
+const IMAGES_SWITCHING = document.querySelector(
+  "div.portfolio__images-switching"
+);
 
-const PORTFOLIO_IMAGES = document.getElementById('presentation');
+const PORTFOLIO_IMAGES = document.getElementById("presentation");
 
 let value = false;
 
@@ -23,144 +25,256 @@ IMAGES_SWITCHING.addEventListener("click", event => {
   );
   event.target.classList.add("images-switching_active");
 
-  function checkImage (position) {
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_1.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '8px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '11px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_2.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '82px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '43px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_3.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '11px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '1px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_4.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '24px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '2px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_5.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '10px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '16px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_6.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '16px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '4px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_7.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '31px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '39px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_8.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '24px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '32px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_9.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '16px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '2px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_10.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '141px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '113x';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_11.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '144px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '175px';
-    } 
-
-    if (PORTFOLIO_IMAGES.children[position].children[0].getAttribute('src') == 'assets/Project_12.png') {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = '36px';
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = '60px';
-    } 
-  }
-  
-   //---------------------Web Design-------------------------------------
-  
-  if (event.target.innerHTML == 'Web Design') {
-    
-     for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-      PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + (i + 2) + '.png')
-      if (i == 11) PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + 1 + '.png')
-      console.log(PORTFOLIO_IMAGES.children[i].children[0].getAttribute('src') == 'assets/Project_1.png')
-      checkImage(i)
-      setInterval(() => {
-         
-      }, 1000)
+  function checkImage(position) {
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_1.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "8px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "11px";
     }
-    
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_2.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "82px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "43px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_3.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "11px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "1px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_4.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "24px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "2px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_5.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "10px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "16px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_6.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "16px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "4px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_7.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "31px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "39px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_8.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "24px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "32px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_9.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "16px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "2px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_10.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "141px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "113x";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_11.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "144px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "175px";
+    }
+
+    if (
+      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
+      "assets/Project_12.png"
+    ) {
+      PORTFOLIO_IMAGES.children[position].children[0].style.right = "36px";
+      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "60px";
+    }
   }
 
-//---------------------All-------------------------------------
+  //---------------------Web Design-------------------------------------
 
-  if (event.target.innerHTML == 'All') {
-    
+  if (event.target.innerHTML == "Web Design") {
     for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-     PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + (i + 1) + '.png')
-     checkImage(i)
-   }
- }
+      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+        "src",
+        "assets/Project_" + (i + 2) + ".png"
+      );
+      if (i == 11)
+        PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+          "src",
+          "assets/Project_" + 1 + ".png"
+        );
 
- //---------------------Graphic Design-------------------------
+      checkImage(i);
+      setInterval(() => {}, 1000);
+    }
+  }
 
- if (event.target.innerHTML == 'Graphic Design') {
-  
-  for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-   PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + (i + 3) + '.png')
-   if (i >= 10) PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + (i - 9) + '.png')
-   checkImage(i)
- }
-}
+  //---------------------All-------------------------------------
 
-//---------------------Artwork--------------------------------
+  if (event.target.innerHTML == "All") {
+    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
+      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+        "src",
+        "assets/Project_" + (i + 1) + ".png"
+      );
+      checkImage(i);
+    }
+  }
 
-if (event.target.innerHTML == 'Artwork') {
-  
-  for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-   PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + (i + 4) + '.png')
-   if (i >= 9) PORTFOLIO_IMAGES.children[i].children[0].setAttribute('src', 'assets/Project_' + (i - 8) + '.png')
-   checkImage(i)
- }
-}
+  //---------------------Graphic Design-------------------------
+
+  if (event.target.innerHTML == "Graphic Design") {
+    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
+      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+        "src",
+        "assets/Project_" + (i + 3) + ".png"
+      );
+      if (i >= 10)
+        PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+          "src",
+          "assets/Project_" + (i - 9) + ".png"
+        );
+      checkImage(i);
+    }
+  }
+
+  //---------------------Artwork--------------------------------
+
+  if (event.target.innerHTML == "Artwork") {
+    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
+      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+        "src",
+        "assets/Project_" + (i + 4) + ".png"
+      );
+      if (i >= 9)
+        PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+          "src",
+          "assets/Project_" + (i - 8) + ".png"
+        );
+      checkImage(i);
+    }
+  }
 });
-
 
 //--------------------------------------------------PORTFOLIO IMAGES----------------------------
 
-PORTFOLIO_IMAGES.addEventListener('click', (event) => {
-  PORTFOLIO_IMAGES.querySelectorAll('li').forEach(elem => elem.classList.remove('presentation_active'));
-  event.target.closest('li').classList.add('presentation_active')
-})
+PORTFOLIO_IMAGES.addEventListener("click", event => {
+  PORTFOLIO_IMAGES.querySelectorAll("li").forEach(elem =>
+    elem.classList.remove("presentation_active")
+  );
+  event.target.closest("li").classList.add("presentation_active");
+});
 
 //-----------------------------------------------BUTTON OF PHONE------------------------------
 
 let switchVerticalScreen = true,
-    switchHorizontalScreen = true;
+  switchHorizontalScreen = true;
 
-document.querySelector('div.phone-vertical__button').addEventListener('click', () => {
-  switchVerticalScreen = !switchVerticalScreen;
-  if (!switchVerticalScreen) {
-    document.querySelector('div.phone-vertical__image').children[0].children[0].style.opacity = '0'
-    document.querySelector('div.phone-vertical__image').children[0].children[0].style.transitionDuration = '0.5s'
-  }
-  else document.querySelector('div.phone-vertical__image').children[0].children[0].style.opacity = '1'
-})
+document
+  .querySelector("div.phone-vertical__button")
+  .addEventListener("click", () => {
+    switchVerticalScreen = !switchVerticalScreen;
+    if (!switchVerticalScreen) {
+      document.querySelector(
+        "div.phone-vertical__image"
+      ).children[0].children[0].style.opacity = "0";
+      document.querySelector(
+        "div.phone-vertical__image"
+      ).children[0].children[0].style.transitionDuration = "0.5s";
+    } else
+      document.querySelector(
+        "div.phone-vertical__image"
+      ).children[0].children[0].style.opacity = "1";
+  });
 
-document.querySelector('div.phone-horizontal__button').addEventListener('click', (event) => {
-  switchHorizontalScreen = !switchHorizontalScreen;
-  if (!switchHorizontalScreen) {
-    document.querySelector('div.phone-horizontal__image').children[0].children[0].style.opacity = '0'
-    document.querySelector('div.phone-horizontal__image').children[0].children[0].style.transitionDuration = '0.5s'
+document
+  .querySelector("div.phone-horizontal__button")
+  .addEventListener("click", event => {
+    switchHorizontalScreen = !switchHorizontalScreen;
+    if (!switchHorizontalScreen) {
+      document.querySelector(
+        "div.phone-horizontal__image"
+      ).children[0].children[0].style.opacity = "0";
+      document.querySelector(
+        "div.phone-horizontal__image"
+      ).children[0].children[0].style.transitionDuration = "0.5s";
+    } else
+      document.querySelector(
+        "div.phone-horizontal__image"
+      ).children[0].children[0].style.opacity = "1";
+  });
+
+//------------------------------------------------REGISTRATION MESSAGE---------------------------
+
+const FORM = document.querySelector("form.registration__contacts-form"),
+  BLOCK_MESSAGE = document.querySelector("div.registration__block-message");
+
+let screenMessage = false,
+  themeMessage = BLOCK_MESSAGE.querySelector("p.message__theme"),
+  descriptionMessage = BLOCK_MESSAGE.querySelector("p.message__description");
+
+FORM.onsubmit = () => {
+  BLOCK_MESSAGE.classList.remove("hidden");
+  if (/^\s*$/.test(FORM.children[2].value))
+    themeMessage.innerHTML = "Without subject";
+  else {
+    themeMessage.innerHTML = "Subject: " + FORM.children[2].value;
   }
-  else document.querySelector('div.phone-horizontal__image').children[0].children[0].style.opacity = '1'
-})
+
+  if (/^\s*$/.test(FORM.children[3].value))
+    descriptionMessage.innerHTML = "Without description";
+  else {
+    descriptionMessage.innerHTML = "Description: " + FORM.children[3].value;
+  }
+  let showMessage = setInterval(() => {
+    BLOCK_MESSAGE.style.opacity = String(
+      +getComputedStyle(BLOCK_MESSAGE).opacity + 0.01
+    );
+  }, 1);
+  setInterval(() => clearInterval(showMessage), 1000);
+
+  return false;
+};
+
+document.querySelector("button.message__btn").onclick = () => {
+  let removeMessage = setInterval(() => {
+    BLOCK_MESSAGE.style.opacity = String(
+      +getComputedStyle(BLOCK_MESSAGE).opacity - 0.01
+    );
+  }, 1);
+  setInterval(() => clearInterval(removeMessage), 1000);
+  setTimeout(() => BLOCK_MESSAGE.classList.add("hidden"), 1000);
+};
