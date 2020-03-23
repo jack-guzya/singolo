fae0b491f29fd71753880973c694ec6fd4c4144a
EvgeniyGuzenkov <jack199479@gmail.com>
2020-03-23 15:41:29 +0300

    feat: add responsive 768px <= width < 1020px

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //----------------------------------------------------NAVIGATION------------------------------------
 
 const NAVIGATION = document.querySelector("ul.navigation"),
-      ANCHORS = document.querySelectorAll('a[href^="#"]');
+  ANCHORS = document.querySelectorAll('a[href^="#"]');
 
 let enabled = true,
-    relativeTopList = {},
-    heightHeader = +getComputedStyle(document.querySelector("header")).height.match(/\-*\d*\.\d*/g).join("") + 6;
+  relativeTopList = {},
+  heightHeader =
+    +getComputedStyle(document.querySelector("header"))
+      .height.match(/\-*\d*\.\d*/g)
+      .join("") + 6;
 
 function getCoordinate(elem) {
   let box = elem.getBoundingClientRect();
@@ -24,21 +27,20 @@ function addActive(event) {
 
 for (let anchor of ANCHORS) {
   if (/#.+/.test(anchor.href)) {
-
     let blockId = anchor.getAttribute("href").substr(1),
-    block = document.getElementById(blockId);
-    
-    relativeTopList[blockId] = block.offsetTop
+      block = document.getElementById(blockId);
+
+    relativeTopList[blockId] = block.offsetTop;
 
     anchor.addEventListener("click", function(event) {
-
       // event to navigation buttons
       event.preventDefault(); // remove default event
       if (!enabled) return;
 
       const blockID = anchor.getAttribute("href").substr(1); //id section
-      let relativelyTop = document.getElementById(blockID).getBoundingClientRect()
-          .top,
+      let relativelyTop = document
+          .getElementById(blockID)
+          .getBoundingClientRect().top,
         step = 1,
         scrollDown,
         scrollUp;
@@ -89,190 +91,98 @@ for (let anchor of ANCHORS) {
 }
 
 document.addEventListener("scroll", () => {
-  for (let anchor in relativeTopList) 
-  if (window.scrollY > relativeTopList[anchor] - heightHeader) addActive ((NAVIGATION.querySelector('a[href^="#' + anchor + '"]')))
+  for (let anchor in relativeTopList)
+    if (window.scrollY > relativeTopList[anchor] - heightHeader)
+      addActive(NAVIGATION.querySelector('a[href^="#' + anchor + '"]'));
 });
-
 
 //--------------------------------------------------PORTFOLIO BUTTONS----------------------------
 
 const IMAGES_SWITCHING = document.querySelector(
-  "div.portfolio__images-switching"
-);
+    "div.portfolio__images-switching"
+  ),
+  PORTFOLIO_BUTTONS = document.querySelectorAll(
+    "div.portfolio__images-switching > button"
+  ),
+  PORTFOLIO_IMAGES = document.getElementById("presentation");
 
-const PORTFOLIO_IMAGES = document.getElementById("presentation");
+let value = true;
 
-let value = false;
+for (let button of PORTFOLIO_BUTTONS) {
+  button.addEventListener("click", event => {
+    if (event.target.className == "images-switching_active") return;
+    if (!value) return;
+    value = false;
+    IMAGES_SWITCHING.querySelectorAll("button").forEach(elem =>
+      elem.classList.remove("images-switching_active")
+    );
+    PORTFOLIO_IMAGES.classList.add("switch");
+    event.target.classList.add("images-switching_active");
+    PORTFOLIO_IMAGES.addEventListener("animationend", () => {
+      PORTFOLIO_IMAGES.classList.remove("switch");
+      value = true;
+    });
 
-IMAGES_SWITCHING.addEventListener("click", event => {
-  IMAGES_SWITCHING.querySelectorAll("button").forEach(elem =>
-    elem.classList.remove("images-switching_active")
-  );
-  event.target.classList.add("images-switching_active");
+    //---------------------Web Design-------------------------------------
 
-  function checkImage(position) {
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_1.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "8px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "11px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_2.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "82px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "43px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_3.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "11px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "1px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_4.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "24px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "2px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_5.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "10px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "16px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_6.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "16px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "4px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_7.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "31px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "39px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_8.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "24px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "32px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_9.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "16px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "2px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_10.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "141px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "113x";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_11.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "144px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "175px";
-    }
-
-    if (
-      PORTFOLIO_IMAGES.children[position].children[0].getAttribute("src") ==
-      "assets/Project_12.png"
-    ) {
-      PORTFOLIO_IMAGES.children[position].children[0].style.right = "36px";
-      PORTFOLIO_IMAGES.children[position].children[0].style.bottom = "60px";
-    }
-  }
-
-  //---------------------Web Design-------------------------------------
-
-  if (event.target.innerHTML == "Web Design") {
-    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
-        "src",
-        "assets/Project_" + (i + 2) + ".png"
-      );
-      if (i == 11)
+    if (event.target.innerHTML == "Web Design") {
+      for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
         PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
           "src",
-          "assets/Project_" + 1 + ".png"
+          "assets/Project_" + (i + 2) + ".png"
         );
-
-      checkImage(i);
-      setInterval(() => {}, 1000);
+        if (i == 11)
+          PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+            "src",
+            "assets/Project_" + 1 + ".png"
+          );
+      }
     }
-  }
 
-  //---------------------All-------------------------------------
+    //---------------------All-------------------------------------
 
-  if (event.target.innerHTML == "All") {
-    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
-        "src",
-        "assets/Project_" + (i + 1) + ".png"
-      );
-      checkImage(i);
-    }
-  }
-
-  //---------------------Graphic Design-------------------------
-
-  if (event.target.innerHTML == "Graphic Design") {
-    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
-        "src",
-        "assets/Project_" + (i + 3) + ".png"
-      );
-      if (i >= 10)
+    if (event.target.innerHTML == "All") {
+      for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
         PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
           "src",
-          "assets/Project_" + (i - 9) + ".png"
+          "assets/Project_" + (i + 1) + ".png"
         );
-      checkImage(i);
+      }
     }
-  }
 
-  //---------------------Artwork--------------------------------
+    //---------------------Graphic Design-------------------------
 
-  if (event.target.innerHTML == "Artwork") {
-    for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
-      PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
-        "src",
-        "assets/Project_" + (i + 4) + ".png"
-      );
-      if (i >= 9)
+    if (event.target.innerHTML == "Graphic Design") {
+      for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
         PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
           "src",
-          "assets/Project_" + (i - 8) + ".png"
+          "assets/Project_" + (i + 3) + ".png"
         );
-      checkImage(i);
+        if (i >= 10)
+          PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+            "src",
+            "assets/Project_" + (i - 9) + ".png"
+          );
+      }
     }
-  }
-});
+
+    //---------------------Artwork--------------------------------
+
+    if (event.target.innerHTML == "Artwork") {
+      for (let i = 0; i < PORTFOLIO_IMAGES.querySelectorAll("li").length; i++) {
+        PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+          "src",
+          "assets/Project_" + (i + 4) + ".png"
+        );
+        if (i >= 9)
+          PORTFOLIO_IMAGES.children[i].children[0].setAttribute(
+            "src",
+            "assets/Project_" + (i - 8) + ".png"
+          );
+      }
+    }
+  });
+}
 
 //--------------------------------------------------PORTFOLIO IMAGES----------------------------
 
